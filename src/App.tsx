1e9a76d61863ada2017e57c40aaf905881f935e2
wstypr <wsetypeermadi@gmail.com>
2024-09-07 22:22:51 +0700

    Add staging CI/CD

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <div>
       <h1 className="text-3xl font-bold ">
-        Hello from <span className="bg-red-600 text-white">/ stagging /</span>
+        Hello from <span className="bg-red-600 text-white">/ staging /</span>
       </h1>
     </div>
   );
