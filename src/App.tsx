cb57d819fb36d2e6fdcf0662e0f2baa27d4e6613
wstypr <wsetypeermadi@gmail.com>
2024-09-08 06:25:14 +0700

    Check staging workflow

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ function App() {
   return (
     <div>
       <h1 className="text-3xl font-bold ">
-        Hello from <span className="bg-red-600 text-white">/ staging /</span>
+        Hello from{" "}
+        <span className="bg-red-600 text-white">/ another staging /</span>
       </h1>
     </div>
   );
